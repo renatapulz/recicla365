a193419748dc03dc27e5c42d3178a4f33b379ed9
Renata Pulz <renata.pulz@raizen.com>
2024-04-25 08:46:32 -0300

    criado menu hamburguer e o header da aplicação - com responsividade

---
+++ src/assets/components/header/header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Menu from "./menuHamburguer/menu";
+import "./style.css";
+
+
+function NavBar() {
+    return (
+        <header className="background-header">
+            <Menu />
+            <div className="container">
+                <div><Link to="/" className="logo">Recicle 365</Link></div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar;
---
+++ src/assets/components/header/menuHamburguer/menu.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+
+function Menu() {
+    const [isOpen, setOpen] = useState(false)
+
+
+    const toggleMenu = () => {
+        setOpen(!isOpen);
+    };
+
+    return (
+        <div>
+            <div className={isOpen ? 'icon iconActive' : 'icon'} onClick={toggleMenu}>
+                <div className="hamburguer hamburguerIcon"></div>
+            </div>
+            <div className={isOpen ? 'menu menuOpen' : 'menu menuClose'}>
+                <div className="list">
+                    <ul className="listItems">
+                        <li><Link to="/" className="link-menu">Pontos de coleta</Link></li>
+                        <li><Link to="/cadastro" className="link-menu">Cadastrar Pontos</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
